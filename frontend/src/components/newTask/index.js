6e82c6aff831c9304f4c52bd7b9ff56a8a630ef1
oscar98colibricode <oscar.alvarez@colibricode.com>
2020-12-14 14:47:56 -0600

    archivos generados y actualizacion de pantalla new task

--- frontend/src/components/newTask/index.js
+++ frontend/src/components/newTask/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import SunEditor from 'suneditor-react';
 import 'suneditor/dist/css/suneditor.min.css';
+import {useDropzone} from 'react-dropzone'
 
 import './style.scss';
 
@@ -9,6 +10,13 @@ const handleSubmit = event => {
 }
 
 const NewTask = () =>{
+    const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+    const files = acceptedFiles.map(file => (
+        <li key={file.path}>
+        {file.path} - {file.size} bytes
+        </li>
+    ));
+
     return(
         <div className="main-container">
             <h3 className="header-page">Create Issue</h3>
@@ -24,7 +32,9 @@ const NewTask = () =>{
                             <option>Issue Type</option>
                         </select>                                          
                         <select className="divided-control" name="priority">
-                            <option>Priority</option>
+                            <option value="High">High</option>
+                            <option value="Medium">Medium</option>
+                            <option value="Low">Low</option>
                         </select>      
                     </div>                    
                     <p className="description">Some issue types are unavailable due to incompatible field configuration and/or workflow associations.</p>
@@ -37,10 +47,30 @@ const NewTask = () =>{
                             <option>Components</option>
                         </select>                                                                    
                     </div>
-                    <div className="input-divider">                        
-                        <input type="text" className="form-control" placeholder="Attachment" name="summary"/>                                                                      
-                    </div>
-                    <div className="input-divider" style={{width: "97%", margin: "1em auto"}}>                    
+                    {/* <div className="input-divider">                                            
+                        <input type="text" className="form-control" placeholder="Attachment" name="summary"/>
+                    </div> */}
+                    
+                    {/* 
+                        Begining of DropZone section
+                     */}
+
+                    <section className="upload-container">                        
+                        <div {...getRootProps({className: 'dropzone'})}>
+                            <input {...getInputProps()} />
+                            <h4 className="lb-description">Attachment</h4>
+                            <p style={{color:"#aeafb1"}}>Drop files here to attach, or <a href="#" style={{color:"#0759bd"}}>browse</a></p>
+                        </div>
+                        <div className="uploaded-files">
+                            <h4>Files</h4>
+                            <ul>{files}</ul>
+                        </div>
+                    </section>
+                    
+                    {/* 
+                        Ending of DropZone section
+                     */}
+                    <div className="input-divider" style={{width: "97%", margin: "2px auto"}}>                    
                         <SunEditor
                             name='description'                            
                             height='250'                        
